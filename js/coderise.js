d0803ba6ca76e95eb6f3a43c26c984f431c40a55
Julian Duque <julianduquej@gmail.com>
2012-11-29 18:24:22 -0500

    Add form example with stackmob

--- js/coderise.js
+++ js/coderise.js
@@ -1,33 +1,78 @@
 (function() {
 
   StackMob.init({
-    appName: "coderise",
-    clientSubdomain: "<YOUR SUBDOMAIN>",
-    publicKey: "<YOUR PUBLIC KEY>",
+    appName: "APPNAME",
+    clientSubdomain: "",
+    publicKey: "",
     apiVersion: 0
   });
+  
+  var Coderiser = StackMob.Model.extend({ schemaName: 'coderisers' });
 
   $('#create-coderiser').click(function(e) {
     e.preventDefault();
+    
+    var name = $('#name').val();
+    var passions = [];
+    $('.passion:checked').each(function(ix, passion) {
+        passions.push($(passion).val());
+    });
+    
+    var entry = new Coderiser({ name: name, passions: passions });
+    entry.create({
+        success: function(model) {
+            var files = $('#avatar')[0].files;
+    
+            for (var i = 0, f; f = files[i]; i++) {
+         
+              var reader = new FileReader();
+         
+              // Closure to capture the file information
+              reader.onload = (function(theFile) {
+                return function(e) {
+         
+                  var base64Content = e.target.result.substring(e.target.result.indexOf(',') + 1, e.target.result.length);
+                  var fileName = theFile.name;
+                  var fileType = theFile.type;
+                   
+                  model.setBinaryFile('avatar', fileName, fileType, base64Content);
+                  model.save();
+                };
+              })(f);
+         
+              // Read in the file as a data URL
+              fileContent = reader.readAsDataURL(f);
+         
+            }
+        },
+        error: function(model, response) {
+            console.log(response);
+        }
+    });
 
-    var Coderiser = StackMob.Model.extend({ schemaName: 'coderisers' });
-    var entry = new Coderiser({ name: 'Coderiser :D', passions: [ 'code', 'fun', 'javascript', 'html', 'css' ] });
-    entry.create();
+    alert("Created a coderiser in StackMob server");
+  });
 
-    console.log("Created a coderiser in StackMob server");
-
-    coderisers = new Coderiser();
+  $('#get-coderisers').click(function(e) {
+      e.preventDefault();
+      
+    var coderisers = new Coderiser();
 
     coderisers.fetch({
-      success: function(model) {
-        console.log(model.toJSON());
-      },
-      error: function(mode, response) {
-        console.log(response);
-      }
+        success: function(model) {
+          var coderisers = model.toJSON();
+          var container = $('#coderisers');
+          container.empty();
+          $.each(coderisers, function(ix, coderiser) {
+              console.log(coderiser);
+              container.append('<p><img src="'+ coderiser.avatar +'" width="60" /> ' + coderiser.name + '</p>');
+          });
+        },
+        error: function(mode, response) {
+            console.log(response);
+        }
     });
 
   });
-
 
 })();
