1a58987b5dfe5c8e41578a6bee06613bdf9c32cc
Julian Duque <julianduquej@gmail.com>
2014-04-01 15:47:37 -0500

    Add basic jQ integration

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,14 @@
+$(function () {
 
+  var signIn = $("#signin");
+
+  signIn.on('click', function (e) {
+    e.preventDefault();
+
+    var username = $("#email").val();
+    var password = $("#password").val();
+
+    console.log(username, password);
+  });
+
+ });
